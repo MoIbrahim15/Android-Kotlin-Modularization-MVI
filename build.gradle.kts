3046742801c19bbeadf4f2aa82268f3cbdf26537
MohamedIbrahim <mohamed.ibrahim15@fci.helwan.edu.eg>
2020-02-14 19:05:14 +0200

    ktlint integration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,26 @@
+import org.jlleitschuh.gradle.ktlint.reporter.ReporterType
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    id(GradlePluginId.KTLINT_GRADLE) version CoreVersion.KTLINT_GRADLE
+}
+
 buildscript {
     repositories {
+        // Android plugin & support libraries
         google()
+
+        // Main open-source repository
         jcenter()
+
+        // Ktlint Gradle
+        maven(GradlePluginId.KTLINT_MAVEN)
     }
     dependencies {
-        classpath (GradleClasspath.ANDROID_GRADLE)
-        classpath (kotlin(GradleClasspath.KOTLIN_PlUGIN, version = CoreVersion.KOTLIN))
-        classpath (GradleClasspath.SAFE_ARGS)
-
-        // NOTE: Do not place your application dependencies here; they belong
-        // in the individual module build.gradle files
+        classpath(GradleClasspath.ANDROID_GRADLE)
+        classpath(kotlin(GradleClasspath.KOTLIN_PlUGIN, version = CoreVersion.KOTLIN))
+        classpath(GradleClasspath.SAFE_ARGS)
+        classpath(GradleClasspath.KTLINT_CLASSPATH)
     }
 }
 
@@ -18,6 +28,22 @@ allprojects {
     repositories {
         google()
         jcenter()
+    }
+
+    // We want to apply ktlint at all project level because it also checks build gradle files
+    plugins.apply(GradlePluginId.KTLINT_GRADLE)
+    // Ktlint configuration for sub-projects
+    ktlint {
+        version.set(CoreVersion.KTLINT)
+        verbose.set(true)
+        android.set(true)
+        reporters {
+            reporter(ReporterType.CHECKSTYLE)
+        }
+        ignoreFailures.set(true)
+        filter {
+            exclude("**/generated/**")
+        }
     }
 }
 
--- buildSrc/src/main/kotlin/constants/GradleDependency.kt
+++ buildSrc/src/main/kotlin/constants/GradleDependency.kt
@@ -2,6 +2,8 @@ object CoreVersion{
     const val KOTLIN = "1.3.61"
     const val NAVIGATION = "2.2.0"
     const val ANDROID_GRADLE = "3.5.3"
+    const val KTLINT_GRADLE = "9.2.1"
+    const val KTLINT = "0.34.2"
 }
 
 object GradlePluginId {
@@ -11,10 +13,13 @@ object GradlePluginId {
     const val ANDROID_EXT = "kotlin-android-extensions"
     const val SAFE_ARGS = "androidx.navigation.safeargs"
     const val BASE_GRADLE_PLUGIN = "base-gradle-plugin"
+    const val KTLINT_GRADLE = "org.jlleitschuh.gradle.ktlint"
+    const val KTLINT_MAVEN =  "https://plugins.gradle.org/m2/"
 }
 
 object GradleClasspath {
     const val KOTLIN_PlUGIN = "gradle-plugin"
     const val ANDROID_GRADLE = "com.android.tools.build:gradle:${CoreVersion.ANDROID_GRADLE}"
     const val SAFE_ARGS = "androidx.navigation:navigation-safe-args-gradle-plugin:${CoreVersion.NAVIGATION}"
+    const val  KTLINT_CLASSPATH ="org.jlleitschuh.gradle:ktlint-gradle:${CoreVersion.KTLINT_GRADLE}"
 }
